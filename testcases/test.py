9b3e7572aaa07d4caeaf0cd0b6fda0113cb76413
xiaoh105 <huruiyan105@126.com>
2024-11-20 12:38:58 +0800

    doc(tests & grammar.md): Add new testcases based on the new grammar rules.

--- testcases/test.py
+++ testcases/test.py
@@ -3,7 +3,7 @@ import os
 if os.path.exists("temp"):
     os.system("rm -rf ./temp")
 os.makedirs("temp")
-for i in range(14):
+for i in range(16):
     inst ="./code < testData/test"+str(i)+".in > temp/test"+str(i)+".out"
     print(inst)
     os.system(inst)
