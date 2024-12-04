8907d407ff1a33059d27aeb57acf96982fd01d3d
bojuzhang <zhangboju@sjtu.edu.cn>
2024-12-04 16:52:37 +0800

    fix varQuery unnecessary construct and back varSet when the varible does not exist

--- src/Scope.cpp
+++ src/Scope.cpp
@@ -6,10 +6,9 @@ void Scope::varRegister(const std::string &varname, std::any vardata) {
 }
 
 std::any Scope::varQuery(const std::string varname) {
-  auto now = varmap_.back();
   // std::cerr << varmap_.size() << "\n";
-  if (!now.empty() && now.find(varname) != now.end()) {
-    return now[varname];
+  if (varmap_.back().find(varname) != varmap_.back().end()) {
+    return varmap_.back()[varname];
   } else {
     return varmap_[0][varname];
   }
@@ -20,14 +19,15 @@ void Scope::varSet(const std::string &varname, std::any vardata) {
   if (!varmap_.back().empty() &&
       varmap_.back().find(varname) != varmap_.back().end()) {
     varmap_.back()[varname] = vardata;
-  } else {
+  } else if (varmap_[0].find(varname) != varmap_[0].end()){
     varmap_[0][varname] = vardata;
+  } else {
+    varmap_.back()[varname] = vardata;
   }
   // std::cerr << "end varset\n";
 }
 
 bool Scope::varFind(const std::string &varname) {
-  return (!varmap_.back().empty() &&
-          varmap_.back().find(varname) != varmap_.back().end()) ||
-         (!varmap_[0].empty() && varmap_[0].find(varname) != varmap_[0].end());
+  return (varmap_.back().find(varname) != varmap_.back().end()) ||
+         (varmap_[0].find(varname) != varmap_[0].end());
 }
